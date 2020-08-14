3c0d2ff5e2a7f9384a6b1b55456d7c200c1a86dd
deepakjha14 <deepakjha14.ex@gmail.com>
2020-08-14 14:49:55 +0530

    some more changes

--- my-element.js
+++ my-element.js
@@ -5,7 +5,7 @@
  * http://polymer.github.io/LICENSE.txt
  * The complete set of authors may be found at
  * http://polymer.github.io/AUTHORS.txt
- * The complete set of contributors may be found at
+ * The complete set of contributors may be found at                
  * http://polymer.github.io/CONTRIBUTORS.txt
  * Code distributed by Google as part of the polymer project is also
  * subject to an additional IP rights grant found at
@@ -25,7 +25,6 @@ export class TopStocks extends LitElement {
     return css`
       :host {
         display: block;
-        border: solid 1px gray;
         padding: 16px;
         max-width: 800px;
       }
@@ -43,13 +42,14 @@ export class TopStocks extends LitElement {
        * The number of times the button has been clicked.
        */
       count: {type: Number},
+      chartData : {type: Array}, 
     };
   }
 
   constructor() {
     super();
     this.chartId = 'ABCCorpContainer';
-    this.chart = '';    
+    this.chart = '';
   }
 
   render() {
@@ -67,12 +67,13 @@ export class TopStocks extends LitElement {
 
   firstUpdated(changedProperties) {
     const chartDiv = this.shadowRoot.getElementById('ABCCorpContainer');
-    this.chart = new CanvasJS.Chart(chartDiv, {
+    this.chart = new CanvasJS.Chart(chartDiv, 
+    {
       animationEnabled: true,
-      exportEnabled: true,
-      theme: "light1", // "light1", "light2", "dark1", "dark2"
+      exportEnabled: false,
+      theme: this.chartData.theme || "light", // "light1", "light2", "dark1", "dark2"
       title:{
-        text: "Simple Column Chart with Index Labels"
+        text: this.chartData.text || "ABC Corp. Graph"
       },
         axisY: {
           includeZero: true
@@ -81,23 +82,9 @@ export class TopStocks extends LitElement {
         type: "column", //change type to bar, line, area, pie, etc
         //indexLabel: "{y}", //Shows y value on all Data Points
         indexLabelFontColor: "#5A5757",
-            indexLabelFontSize: 16,
+        indexLabelFontSize: 16,
         indexLabelPlacement: "outside",
-        dataPoints: [
-          { x: 10, y: 71 },
-          { x: 20, y: 55 },
-          { x: 30, y: 50 },
-          { x: 40, y: 65 },
-          { x: 50, y: 92, indexLabel: "\u2605 Highest" },
-          { x: 60, y: 68 },
-          { x: 70, y: 38 },
-          { x: 80, y: 71 },
-          { x: 90, y: 54 },
-          { x: 100, y: 60 },
-          { x: 110, y: 36 },
-          { x: 120, y: 49 },
-          { x: 130, y: 21, indexLabel: "\u2691 Lowest" }
-        ]
+        dataPoints: this.chartData.plotData
       }]
     });
   }
